9e2308a94d2dbfe541dcf2a31adb3285480037e2
ELTEGANI <tiganimoh@gmail.com>
2022-10-02 17:38:57 +0400

    setup crypto manger

--- app/src/main/java/com/plcoding/androidcrypto/CryptoManger.kt
+++ app/src/main/java/com/plcoding/androidcrypto/CryptoManger.kt
@@ -12,35 +12,35 @@ import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
 
-@RequiresApi(Build.VERSION_CODES.M)
-class CryptoManager {
 
+@RequiresApi(Build.VERSION_CODES.M)
+class CryptoManger {
     private val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {
         load(null)
     }
 
-    private val encryptCipher get() = Cipher.getInstance(TRANSFORMATION).apply {
-        init(Cipher.ENCRYPT_MODE, getKey())
+    private  val  encryptCipher = Cipher.getInstance(TRANSFORMATION).apply {
+       init(Cipher.ENCRYPT_MODE,getKey())
     }
 
-    private fun getDecryptCipherForIv(iv: ByteArray): Cipher {
+
+    private fun getKey():SecretKey{
+       val existingKey = keyStore.getEntry("secret",null) as? KeyStore.SecretKeyEntry
+       return existingKey?.secretKey ?: createKey()
+    }
+
+
+    private fun getDecryptCipherForIv(iv: ByteArray):Cipher{
         return Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.DECRYPT_MODE, getKey(), IvParameterSpec(iv))
+            init(Cipher.DECRYPT_MODE,getKey(),IvParameterSpec(iv))
         }
     }
-
-    private fun getKey(): SecretKey {
-        val existingKey = keyStore.getEntry("secret", null) as? KeyStore.SecretKeyEntry
-        return existingKey?.secretKey ?: createKey()
-    }
-
-    private fun createKey(): SecretKey {
+    private fun createKey():SecretKey{
         return KeyGenerator.getInstance(ALGORITHM).apply {
             init(
                 KeyGenParameterSpec.Builder(
                     "secret",
-                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-                )
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
                     .setBlockModes(BLOCK_MODE)
                     .setEncryptionPaddings(PADDING)
                     .setUserAuthenticationRequired(false)
@@ -50,8 +50,8 @@ class CryptoManager {
         }.generateKey()
     }
 
-    fun encrypt(bytes: ByteArray, outputStream: OutputStream): ByteArray {
-        val encryptedBytes = encryptCipher.doFinal(bytes)
+    fun encrypt(bytes:ByteArray,outputStream:OutputStream):ByteArray{
+       val encryptedBytes = encryptCipher.doFinal(bytes)
         outputStream.use {
             it.write(encryptCipher.iv.size)
             it.write(encryptCipher.iv)
@@ -61,25 +61,22 @@ class CryptoManager {
         return encryptedBytes
     }
 
-    fun decrypt(inputStream: InputStream): ByteArray {
-        return inputStream.use {
-            val ivSize = it.read()
-            val iv = ByteArray(ivSize)
-            it.read(iv)
+    fun decrypt(inputStream: InputStream):ByteArray{
+       return  inputStream.use {
+           val ivSize = it.read()
+           val iv = ByteArray(ivSize)
+           it.read(iv)
+           val encryptedBytesSize = it.read()
+           val encrptedBytes = ByteArray(encryptedBytesSize)
+           it.read(encrptedBytes)
 
-            val encryptedBytesSize = it.read()
-            val encryptedBytes = ByteArray(encryptedBytesSize)
-            it.read(encryptedBytes)
-
-            getDecryptCipherForIv(iv).doFinal(encryptedBytes)
-        }
+           getDecryptCipherForIv(iv).doFinal(encrptedBytes)
+       }
     }
-
-    companion object {
+    companion object{
         private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
         private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
         private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
-        private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
+        private const val TRANSFORMATION = "$ALGORITHM/BLOCK_MODE/PADDING"
     }
-
 }
